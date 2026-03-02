3638463282bd99b39c23243d9f001be67ef88467
boobalan k <boobalantechno@gmail.com>
2026-03-02 20:40:50 +0530

    fix static files with whitenoise

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,16 +117,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/' # for static files like css,js,icons (developer files)
+STATIC_URL = '/static/' # for static files like css,js,icons (developer files)
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,"static")
+# old version of dirs 
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,"static") 
+# ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
 ]
 
-MEDIA_URL='media/' #for media files like image , document (user files)
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/' #for media files like image , document (user files)
+MEDIA_ROOT= BASE_DIR / 'media'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGIN_URL = 'login'
 SESSION_COOKIE_AGE = 3600
